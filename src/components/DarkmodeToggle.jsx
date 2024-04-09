7a4dea2c894c5b9be36c717f6e78f873e094fad2
lassespilling <lassespilling@gmail.com>
2024-04-09 18:40:22 +0200

    fix cmdk

--- src/components/DarkmodeToggle.jsx
+++ src/components/DarkmodeToggle.jsx
@@ -1,12 +1,22 @@
 import { useStore } from '@nanostores/react';
 import { darkmode } from '../globalState';
+import { useEffect } from 'react';
 
-const DarkmodeToggle = () => {
+const DarkmodeToggle = ({ override }) => {
     // read the store value with the `useStore` hook
     const $darkmode = useStore(darkmode);
+    useEffect(() => {
+        if (override) {
+            darkmode.set(override);
+        }
+    }, [override]);
+
     // write to the imported store using `.set`
     return (
-        <button onClick={() => darkmode.set(!$darkmode)} style={{ padding: "1rem", width: "max-content" }}>TOGGLE DARKMODE</button>
+        <>
+            <pre>{JSON.stringify(override, null, 1)}</pre>
+            <button onClick={() => darkmode.set(!$darkmode)} style={{ padding: "1rem", width: "max-content" }}>TOGGLE DARKMODE</button>
+        </>
     );
 };
 
